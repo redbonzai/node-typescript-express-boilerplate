32bbf3d8b77e84832eb8fa1b36ec3fda91d2d92a
Christian Dominguez <christian.burke@7-11.com>
2019-07-02 18:48:49 -0500

    ErrorHandler:  Finish error handling, and use the error handler inside the request objects.

--- api/tours/apiCreateTour.ts
+++ api/tours/apiCreateTour.ts
@@ -1,8 +1,15 @@
+import { APIError, PublicInfo } from './../../models/shared/messages';
 import { DataStore } from './../../data/data';
 import { RequestHandler } from 'express';
 import uuid from 'uuid/v4';
 
 export const apiCreateTour: RequestHandler = (req, res, next) => {
+    const requiredFields = ['tourTitle', 'location'];
+    const givenFields = Object.getOwnPropertyNames(req.body);
+
+    if (!requiredFields.every(field => givenFields.includes(field))) {
+        return next(new APIError('data missing', 'Not all required fields supplied', 400));
+    }
     const newTour = {
         id: uuid(),
         location: req.body.location || '',
@@ -15,5 +22,5 @@ export const apiCreateTour: RequestHandler = (req, res, next) => {
     }
 
     DataStore.tours.push(newTour);
-    res.send("New Tour Created");
+    res.json(new PublicInfo('Tour Successfully added', 200, {tour: newTour}));
 }
